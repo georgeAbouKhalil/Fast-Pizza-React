23df58a17fcfdd43f03fad0a75dad2d7598b3533
George <88317294+georgeAbouKhalil@users.noreply.github.com>
2024-04-16 22:18:37 -0400

    1st

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+const config = {
+  // plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+};
+
+export default config;
---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
+import { useFetcher } from 'react-router-dom';
+import Button from '../../ui/Button';
+import { updateOrder } from '../../services/apiRestaurant';
+
+// eslint-disable-next-line react/prop-types
+export default function UpdateOrder({ order }) {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make priority</Button>
+    </fetcher.Form>
+  );
+}
+
+export async function action({ request, params }) {
+  const data = { priority: true };
+  await updateOrder(params.orderId, data);
+  return null;
+}
